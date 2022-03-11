b48891d67ae388d6d13897b4dea30beca4f98e8f
Reed Meyer <emailreedmeyer@gmail.com>
2022-03-10 18:11:35 -0600

    Initial commit

---
+++ calibrationMaster.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from CameraCalibration import *
+from main import *
+import pickle
+print('please place calibration sheet in left bottom corner and press space')
+# 1) take photo from spacebar input and put into directory
+# 2) =... print('please place calibration sheet in right bottom corner and press space')
+# 3) run Main() from CameraCalibration.py, and pass main(inputImage, name of directory)
+# 4) spits out calibrated image to 'images/default.png' or 'images/secondary.png'
---
+++ master.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from CameraCalibration import *
+from main import *
+import pickle
+
+objfile = open('objpoints.pkl', 'rb')
+objpoints = pickle.load(objfile)
+objfile.close()
+
+imgfile = open('imgpoints.pkl', 'rb')
+imgpoints = pickle.load(imgfile)
+imgfile.close()
+
+Calibrate(objpoints, imgpoints, (2122,3226), 'images/blue_scan_300.jpg')
+
+measurement('images/default.png')
+
